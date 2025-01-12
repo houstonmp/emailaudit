53de28c9c2c04237a0c6f79b205be52078d3b60a
Matthew Houston <matthouston027@gmail.com>
2025-01-12 01:18:08 -0600

    Add comma and space after emails

--- src/components/RecipientsDisplay.tsx
+++ src/components/RecipientsDisplay.tsx
@@ -1,7 +1,15 @@
 export interface RecipientProps {
-    recipients: string
+    recipients: string[]
 }
 
 export default function RecipientsDisplay({ recipients }: RecipientProps) {
-    console.log(recipients)
+    return (<span>{recipients.map((el: string, i: number) => {
+        if (i + 1 === recipients.length) {
+            return el
+        }
+        else {
+            console.log("email: " + el, "index: " + i)
+            return el + ', '
+        }
+    })}</span>)
 }
